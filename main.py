6cd77844729408bab74d698c21683f9cbe914ca5
skwinkelenberg <s.kwinkelenberg@student.tudelft.nl>
2021-10-31 14:49:17 +0100

    It should work now

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from os import scandir
 import praw
 from praw.models import MoreComments
+from time import time
 
 # Parameters
-NUM_POSTS = 30
+NUM_POSTS = 63
 BOT_FILE = "bot_data/bot_list"
 SCANNED_FILE = "bot_data/scanned_posts"
 SCORE_LIMIT = 0
+MIN_POST_AGE = 10
+
+REPLY = True
+
+def check_post_age(post):
+    created = post.created_utc
+    
+    # post_age in hours
+    post_age = (time() - created)/3600
+
+    return post_age
 
 def get_scanned_posts():
     with open(SCANNED_FILE, 'r') as f:
@@ -49,6 +61,9 @@ def get_double_bot_comments(post, ca_dict):
                     comment_parent_author = ca_dict[comment.parent_id[3:]]
                     if comment_parent_author in bot_list:
                         #print(f"{comment.author} responded to {comment_parent_author} with \"{comment.body}\"")
+                        if REPLY:
+                            comment.reply("This is getting out of hand, now there are two of them!")
+                        
                         count +=1
     return count
 
@@ -62,10 +77,13 @@ bot_list = get_bot_list()
 scanned_posts = get_scanned_posts()
 
 # Start scanning
-for post in reddit.subreddit("prequelmemes").hot(limit=NUM_POSTS):
+for post in reddit.subreddit("prequelmemes").new(limit=NUM_POSTS):
 
     if post.id in scanned_posts:
         print("already scanned")
+        pass
+    elif check_post_age(post) < MIN_POST_AGE:
+        print(f"post too young, it is only {check_post_age(post)} hours old")
         pass
     else:
         print(f"Starting examining post with title \"{post.title}\"")
